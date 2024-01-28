a158dc23ececa8a22ba7f2e26e9ede5532f41c1c
joonyoungchoi0801 <151784621+joonyoungchoi0801@users.noreply.github.com>
2024-01-28 16:13:58 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 56.86 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+let star = '';
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    for(let i =1;i<=input[0];i++){
+        star += '*'
+        console.log(star);
+    }
+});
