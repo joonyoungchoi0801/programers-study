62dac24bf844e51f5ccd8909e57e4ecfe37a196e
joonyoungchoi0801 <151784621+joonyoungchoi0801@users.noreply.github.com>
2024-02-26 02:34:58 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    for(let str of my_string){
+        if(str === str.toUpperCase()){
+            answer+=str.toLowerCase();
+        }
+        else{
+            answer+=str.toUpperCase();
+        }
+    }
+    return answer;
+}
