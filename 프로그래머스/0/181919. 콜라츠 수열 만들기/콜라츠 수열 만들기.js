b9f0b67381f50b95cb3e371c0159b04c40afbecb
joonyoungchoi0801 <151784621+joonyoungchoi0801@users.noreply.github.com>
2024-03-27 01:00:38 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [];
+    while(n>1){
+        
+        if(n%2===0){
+            answer.push(n);
+            n /= 2;
+        }
+        else{
+            answer.push(n);
+            n = 3*n+1;
+        }
+    }
+    answer.push(1);
+    return answer;
+}
