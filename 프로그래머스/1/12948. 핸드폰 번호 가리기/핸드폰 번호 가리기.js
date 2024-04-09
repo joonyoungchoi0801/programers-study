d75172c8f5462c87cb29e953756ae5f2cc40854d
joonyoungchoi0801 <151784621+joonyoungchoi0801@users.noreply.github.com>
2024-04-09 14:23:54 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,13 @@
+function solution(phone_number) {
+    var answer = '';
+    let len =phone_number.length;
+    for(let i =0;i<len;i++){
+        if(i<len-4){
+            answer+='*';
+        }
+        else{
+            answer+=phone_number[i];
+        }
+    }
+    return answer;
+}
