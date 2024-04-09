9cb77d7506e5e65ff74d04039133ea89a87b2b12
joonyoungchoi0801 <151784621+joonyoungchoi0801@users.noreply.github.com>
2024-04-09 20:11:15 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = '';
+    for(let str of my_string){
+        if(!answer.includes(str)){
+            answer+=str;
+        }
+    }
+    return answer;
+}
